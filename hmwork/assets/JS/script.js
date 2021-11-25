59c8af0e5dd54a01f0d0c19f31a5adbf9c920c53
MezemovRufetCode <90691159+MezemovRufetCode@users.noreply.github.com>
2021-11-25 23:36:48 +0400

    Add files via upload

--- hmwork/assets/JS/script.js
+++ hmwork/assets/JS/script.js
@@ -1,13 +1,16 @@
-let buttons = document.querySelectorAll(".btn");
-let heading = document.querySelector(".heading");
-let info = document.querySelector(".info");
-let image = document.querySelector(".image");
-buttons.forEach((btn) => {
-  btn.onclick = function () {
-    let headId = btn.getAttribute("data-id");
-    heading.innerHTML = headId;
-    info.innerHTML = "Salam";
-    heading.innerHTML = headId;
-    btn.style.backgroundColor = "rgb(67, 177, 180";
+let buttons = document.querySelectorAll(".btnn");
+buttons.forEach((button) => {
+  button.onclick = function () {
+    let dataId = button.getAttribute("dataid");
+    buttons.forEach((btn) => {
+      btn.classList.remove("active");
+    });
+    let content = document.getElementById(dataId);
+    let contents = document.querySelectorAll(".city");
+    contents.forEach((cty) => {
+      cty.classList.add("d-none");
+    });
+    button.classList.add("active");
+    content.classList.remove("d-none");
   };
 });
